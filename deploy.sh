f31fa89de966da1900f21dd6503c417d71cdad30
chunga7879 <chunga7879@gmail.com>
2022-04-29 03:05:15 -0700

    move to this account

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 
 npm run build
 
-rclone --config rclone.config --update --use-server-modtime --fast-list --progress sync build S3:hairless.brycemw.ca
+rclone *
 # Used to cleanup unfinished uploads
 #rclone --config rclone.config cleanup S3:hairless.brycemw.ca
